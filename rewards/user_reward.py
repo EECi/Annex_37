a5d68cc9122a609579947b073d74262b199cc854
Vijja Wichitwechkarn <wvijja@gmail.com>
2023-01-19 20:55:02 +0000

    further simplification

--- rewards/user_reward.py
+++
@@ -1,6 +0,0 @@
-from citylearn.reward_function import RewardFunction
-
-
-class UserReward(RewardFunction):
-    def __init__(self, agent_count, **kwargs):
-        super().__init__(agent_count, **kwargs)
