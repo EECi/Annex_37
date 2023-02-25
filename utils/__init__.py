a55d7aaf2742391e88a88830a67ca30883fc8924
mal84emma <mal84@cam.ac.uk>
2023-02-25 11:51:45 +0000

    Clean up relative import

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,4 +2,4 @@
 A series of helpful utitlity functions for interacting with the provided datasets.
 """
 
-from utils.timeseries import interactive_timeseries_plot
+from .timeseries import interactive_timeseries_plot
