870b2405fa19b6aa8d2d920ed1320d9318afa905
Vijja Wichitwechkarn <wvijja@gmail.com>
2023-03-29 23:00:22 +0100

    switching branches so i need to commit...

--- inference.py
+++ inference.py
@@ -3,9 +3,9 @@ from utils.dms import IndividualInference
 
 
 building_index = 5
-dataset_type = 'load'
+dataset_type = 'carbon'
 
-expt_name = 'linear_L168_T48'
+expt_name = 'linear_L168_T168'
 predictor = Predictor(expt_name=expt_name, load=True)
 _, pred, gt, gt_t, pred_t, mse = predictor.test_individual(building_index, dataset_type)
 print(f'mse = {mse}')
