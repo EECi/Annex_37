7deff09528abef5b09304a97c204fff338f6c3b4
mal84emma <mal84@cam.ac.uk>
2023-03-29 15:32:47 +0100

    Update evaluation method to accommodate new metric choices & document

--- evaluate.py
+++ evaluate.py
@@ -16,7 +16,30 @@ from linmodel import LinProgModel
 from predictor import Predictor
 
 
-def evaluate(schema_path, tau, **kwargs):
+def evaluate(schema_path,
+    tau,
+    objective_dict = {'price':True,'carbon':True,'ramping':True},
+    clip_level = 'd',
+    **kwargs
+    ):
+    """Evaluate performance of LinMPC controller with given Predictor model.
+
+    Args:
+        schema_path (Str or os.Path): path to schema defining simulation data.
+        tau (int): length of planning horizon
+        objective_dict (dict, optional): dictionary defining objective contributions
+        to use in LinMPC objective. Defaults to {'price':True,'carbon':True,'ramping':True}.
+        clip_level (str, optional): 'd' (district) or 'b' (building). Level at which to clip
+        power values when computing costs for objective. For 'd', grid power flows are clipped,
+        meaning the costs are those for the overall portfolio of buildings, allowing energy
+        transfer between building. For 'b', building power flows are clipped and the costs are
+        the mean building level costs, assuming no interaction effects/cost coordination. Defaults to 'd'.
+
+    Returns:
+        results (dict): dictionary containing costs (contributions & overall) from model evaluation, and
+        forecasting & LP solve time.
+    """
+
     print("Starting evaluation.")
 
     # Initialise CityLearn environment object.
@@ -26,7 +49,7 @@ def evaluate(schema_path, tau, **kwargs):
     lp = LinProgModel(env=env)
     lp.set_battery_propery_data()
     lp.tau = tau
-    lp.generate_LP()
+    lp.generate_LP(objective_dict=objective_dict, clip_level=clip_level)
 
     # Initialise Predictor object.
 
@@ -44,7 +67,8 @@ def evaluate(schema_path, tau, **kwargs):
     done = False
 
     observations = env.reset()
-    current_socs = np.array([charge*capacity for charge,capacity in zip(np.array(observations)[:,22],lp.battery_capacities)]) # get initial SoCs
+    soc_obs_index = 22
+    current_socs = np.array([charge*capacity for charge,capacity in zip(np.array(observations)[:,soc_obs_index],lp.battery_capacities)]) # get initial SoCs
 
     # Execute control loop.
     while not done:
@@ -80,39 +104,63 @@ def evaluate(schema_path, tau, **kwargs):
 
         # Update battery states-of-charge
         # ====================================================================
-        current_socs = np.array([charge*capacity for charge,capacity in zip(np.array(observations)[:,22],lp.battery_capacities)])
+        current_socs = np.array([charge*capacity for charge,capacity in zip(np.array(observations)[:,soc_obs_index],lp.battery_capacities)])
 
         num_steps += 1
 
     print("Evaluation complete.")
 
-
-    metrics = env.evaluate()  # Provides a break down of other metrics that might be of interest.
+    metrics = env.evaluate()  # provides a break down of other metrics that might be of interest.
     if np.any(np.isnan(metrics['value'])):
         raise ValueError("Some of the metrics returned are NaN, please contact organizers.")
 
+    if clip_level == 'd': # manually compute old metrics
+        price_cost = np.sum(np.maximum(env.net_electricity_consumption_price,0))/np.sum(np.maximum(env.net_electricity_consumption_without_storage_price,0))
+        emissions_cost = np.sum(np.maximum(env.net_electricity_consumption_emission,0))/np.sum(np.maximum(env.net_electricity_consumption_without_storage_emission,0))
+
+    elif clip_level == 'b': # get new metrics from env evaluation method
+        price_cost = metrics.iloc[5].value if objective_dict['price'] else np.NaN
+        emissions_cost = metrics.iloc[2].value if objective_dict['carbon'] else np.NaN
+
+    grid_cost = np.mean([metrics.iloc[0].value, metrics.iloc[6].value]) if objective_dict['ramping'] else np.NaN
+    overall_cost = np.nanmean([price_cost,emissions_cost,grid_cost])
+
     print("=========================Results=========================")
-    print(f"Price Cost: {metrics.iloc[5].value}")
-    print(f"Emission Cost: {metrics.iloc[2].value}")
-    print(f"Grid Cost: {np.mean([metrics.iloc[0].value, metrics.iloc[6].value])}")
-    print(f"Total time taken by Predictor: {forecast_time_elapsed}s")
-    print(f"Total time taken by LP solver: {lp_solver_time_elapsed}s")
+    print(f"Price Cost: {round(price_cost,5)}")
+    print(f"Emissions Cost: {round(emissions_cost,5)}")
+    print(f"Grid Cost: {round(grid_cost,5)}")
+    print(f"Overall Cost: {round(overall_cost,5)}")
+    print(f"Total time taken by LP solver: {round(lp_solver_time_elapsed,1)}s")
+
+    results = {
+        'Price Cost':price_cost,
+        'Emissions Cost':emissions_cost,
+        'Grid Cost':grid_cost,
+        'Overall Cost':overall_cost,
+        'Forecast Time': forecast_time_elapsed,
+        'Solve Time':lp_solver_time_elapsed
+    }
 
     # ========================================================================
     # insert your logging code here
     # ========================================================================
 
+    return results
 
 
 if __name__ == '__main__':
     import warnings
 
-    tau = 12 # model prediction horizon (number of timesteps of data predicted)
     dataset_dir = os.path.join('example','test') # dataset directory
 
     schema_path = os.path.join('data',dataset_dir,'schema.json')
 
+    objective_dict = {'price':True,'carbon':True,'ramping':False}
+    clip_level = 'b' # aggregation level for objective
+
+    tau = 12 # model prediction horizon (number of timesteps of data predicted)
+
     with warnings.catch_warnings():
         warnings.filterwarnings(action='ignore',module=r'cvxpy')
 
-        evaluate(schema_path, tau)
+        results = evaluate(schema_path, tau, objective_dict, clip_level)
