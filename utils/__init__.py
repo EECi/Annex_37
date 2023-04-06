cd8ae64b71e31b755a27fdbc4e38fe75896e73bd
mal84emma <mal84@cam.ac.uk>
2023-04-06 16:13:37 +0100

    Spelling correction

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,5 @@
 """
-A series of helpful utitlity functions for interacting with the provided datasets.
+A series of helpful utility functions for interacting with the provided datasets.
 """
 
 from .timeseries import interactive_timeseries_plot
