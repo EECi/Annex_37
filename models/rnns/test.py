15a540a316deffe6c52a4bdcbc8c631ad9d8900b
Max Langtry <89249742+mal84emma@users.noreply.github.com>
2023-07-26 11:32:41 +0100

    Revert "Chaoqun/rnn"

--- models/rnns/test.py
+++
@@ -1,6 +0,0 @@
-from models.dms.predictor import Predictor
-
-results_file = 'results.csv'
-expt_name = 'H256_L168_T48'
-predictor = Predictor(expt_name=expt_name, load=True, results_file=results_file)
-header, results = predictor.test()
