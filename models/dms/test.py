870657611418f441b737421ccedd6833ddba9a7b
mal84emma <mal84@cam.ac.uk>
2023-08-03 22:29:46 +0100

    Set up code for batch training

--- models/dms/test.py
+++ models/dms/test.py
@@ -1,6 +1,9 @@
+import os
 from models.dms.predictor import Predictor
 
+building_indices = [0,3,9,11,12,15,16,25,26,32,38,44,45,48,49]
+
 results_file = 'results.csv'
-expt_name = 'C5K6O6P6'
-predictor = Predictor(expt_name=expt_name, load=True, results_file=results_file)
+expt_name = os.path.join('analysis','linear_1')
+predictor = Predictor(building_indices=building_indices, expt_name=expt_name, load=True, results_file=results_file)
 header, results = predictor.test()
