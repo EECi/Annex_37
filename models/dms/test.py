fb0b7e6fa77d5dd475fbf96853ba1a680e48cb8b
mal84emma <mal84@cam.ac.uk>
2023-08-08 20:10:54 +0100

    Test multiple models

--- models/dms/test.py
+++ models/dms/test.py
@@ -3,7 +3,8 @@ from models.dms.predictor import Predictor
 
 building_indices = [0,3,9,11,12,15,16,25,26,32,38,44,45,48,49]
 
-results_file = 'results.csv'
-expt_name = os.path.join('analysis','linear_1')
-predictor = Predictor(building_indices=building_indices, expt_name=expt_name, load=True, results_file=results_file)
-header, results = predictor.test()
+for model in ['linear_0','resmlp_0','conv_0']:
+    results_file = 'results.csv'
+    expt_name = os.path.join('analysis',model)
+    predictor = Predictor(building_indices=building_indices, expt_name=expt_name, load=True, results_file=results_file)
+    header, results = predictor.test()
