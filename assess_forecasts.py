Correct & check solar generation units!!!

assess_forecasts.py
@@ -91,7 +91,7 @@ def assess(predictor, schema_path, tau, building_breakdown=False, **kwargs):
 
     # Initialise logging objects.
     load_logs = {b.name:{'forecasts': [], 'actuals': []} for b in env.buildings}
-    pv_gen_logs = {b.name:{'forecasts': [], 'actuals': []} for b in env.buildings}
+    pv_gen_logs = {'forecasts': [], 'actuals': []}
     pricing_logs = {'forecasts': [], 'actuals': []}
     carbon_logs = {'forecasts': [], 'actuals': []}
 
@@ -128,14 +128,14 @@ def assess(predictor, schema_path, tau, building_breakdown=False, **kwargs):
                 # Log forecasts.
                 for i, b in enumerate(env.buildings):
                     load_logs[b.name]['forecasts'].append(forecasts[0][i].reshape(-1))
-                    pv_gen_logs[b.name]['forecasts'].append(forecasts[1][i].reshape(-1))
+                pv_gen_logs['forecasts'].append(forecasts[1].reshape(-1))
                 pricing_logs['forecasts'].append(forecasts[2].reshape(-1))
                 carbon_logs['forecasts'].append(forecasts[3].reshape(-1))
                 # Log ground-truth values.
                 # note abuse of Python array slicing to give variable length actuals toward end of lists
                 for i,b in enumerate(env.buildings):
                     load_logs[b.name]['actuals'].append(b.energy_simulation.non_shiftable_load[env.time_step+1:env.time_step+1+tau])
-                    pv_gen_logs[b.name]['actuals'].append(b.energy_simulation.solar_generation[env.time_step+1:env.time_step+1+tau])
+                pv_gen_logs['actuals'].append(b.energy_simulation.solar_generation[env.time_step+1:env.time_step+1+tau])
                 pricing_logs['actuals'].append(b.pricing.electricity_pricing[env.time_step+1:env.time_step+1+tau])
                 carbon_logs['actuals'].append(b.carbon_intensity.carbon_intensity[env.time_step+1:env.time_step+1+tau])
 
@@ -162,13 +162,9 @@ def assess(predictor, schema_path, tau, building_breakdown=False, **kwargs):
         for mname in metric_names}
 
     pv_gen_metrics = {
-        b.name:{
-            mname: compute_metric_score(pv_gen_logs[b.name]['forecasts'],pv_gen_logs[b.name]['actuals'],metric,gnorm)\
-                for metric,mname,gnorm in zip(metrics,metric_names,globally_mean_normalised)
-        } for b in env.buildings
-    }
-    pv_gen_metrics['buildings_average'] = {mname: np.mean([pv_gen_metrics[b.name][mname] for b in env.buildings])\
-        for mname in metric_names}
+            mname: compute_metric_score(pv_gen_logs['forecasts'],pv_gen_logs['actuals'],metric)\
+                for metric,mname in zip(metrics,metric_names)
+        }
 
     pricing_metrics = {
             mname: compute_metric_score(pricing_logs['forecasts'],pricing_logs['actuals'],metric)\
@@ -187,7 +183,7 @@ def assess(predictor, schema_path, tau, building_breakdown=False, **kwargs):
     print("---Load---")
     for mname in metric_names: print(f"{mname}: {round(load_metrics['buildings_average'][mname],5)}")
     print("---Solar Generation---")
-    for mname in metric_names: print(f"{mname}: {round(pv_gen_metrics['buildings_average'][mname],5)}")
+    for mname in metric_names: print(f"{mname}: {round(pv_gen_metrics[mname],5)}")
     print("=====Pricing=====")
     for mname in metric_names: print(f"{mname}: {round(pricing_metrics[mname],5)}")
     print("=====Carbon Intensity=====")
@@ -198,8 +194,6 @@ def assess(predictor, schema_path, tau, building_breakdown=False, **kwargs):
             print(f"====={b.name}=====")
             print("---Load---")
             for mname in metric_names: print(f"{mname}: {round(load_metrics[b.name][mname],5)}")
-            print("---Solar Generation---")
-            for mname in metric_names: print(f"{mname}: {round(pv_gen_metrics[b.name][mname],5)}")
 
     results = {
         'Load Forecasts': load_metrics,

evaluate.py
@@ -51,7 +51,7 @@ def evaluate(predictor,
     env = CityLearnEnv(schema=schema_path)
 
     # Initialise Predictor object.
-    if type(predictor) in [TFT_Predictor, NHiTS_Predictor, DeepAR_Predictor, LSTM_Predictor, GRU_Predictor]:
+    if type(predictor) in [ExamplePredictor, TFT_Predictor, NHiTS_Predictor, DeepAR_Predictor, LSTM_Predictor, GRU_Predictor]:
         predictor.initialise_forecasting(tau, env)
 
     # Initialise Linear MPC object.
@@ -97,15 +97,9 @@ def evaluate(predictor,
             else:
                 forecasts = list(forecasts)
                 forecasts[0] = forecasts[0].reshape(len(env.buildings), -1)
-                forecasts[1] = forecasts[1].reshape(len(env.buildings), -1)
+                forecasts[1] = np.array([b.pv.get_generation(forecasts[1]) for b in env.buildings])
                 forecasts[2] = forecasts[2].reshape(-1)
                 forecasts[3] = forecasts[3].reshape(-1)
-
-                # ================================================================
-                # temp fix, avoid solar forecasts and set to be perfect predictions
-                forecasts[1] = np.array(
-                    [b.pv.get_generation(b.energy_simulation.solar_generation)[num_steps+1:num_steps+tau+1]\
-                        for b in env.buildings])
                 # ================================================================
 
                 # setup and solve predictive Linear Program model of system

models/TFModels/TFPredictor.py
@@ -79,7 +79,7 @@ class TF_Predictor(BasePredictorModel):
             T (int, optional): Model group planning horizon parameter. Defaults to 48.
             model_names (Union[List, Dict], optional): Dictionary of names of models of
             each type to load, or list of indices to quickly specify models named `load_{index}`,
-            `solar_{index}`, `pricing`, `carbon`. Defaults to None. Not required is loading a
+            `solar`, `pricing`, `carbon`. Defaults to None. Not required is loading a
             model group.
             load (Union[str, bool], optional): Whether to load a model group ('group'),
             load a custom selection of models ('indiv') as specified in the `model_names`
@@ -123,7 +123,7 @@ class TF_Predictor(BasePredictorModel):
             assert all([type(item) == int for item in model_names])
             model_names = {
                 'load': [f'load_{b}' for b in model_names],
-                'solar': [f'solar_{b}' for b in model_names],
+                'solar': 'solar',
                 'pricing': 'pricing',
                 'carbon': 'carbon'
             }
@@ -133,19 +133,19 @@ class TF_Predictor(BasePredictorModel):
             if not load: # initialise blank dict of model names to be filled later
                 model_names = {
                     'load': [],
-                    'solar': [],
+                    'solar': None,
                     'pricing': None,
                     'carbon': None
                 }
             elif load == 'group': # get model names for given `model_group_name`
                 for model_type in self.model_types:
                     assert os.path.exists(os.path.join(self.model_group_path,model_type)), f"`{model_type}` model logs sub-dir does not exist."
-                for model_type in ['pricing','carbon']:
+                for model_type in ['solar','pricing','carbon']:
                     if len(list(os.scandir(os.path.join(self.model_group_path,model_type)))) > 1:
                         warnings.warn("Warning: More than 1 {} model available. 1st selected in group load.".format(model_type))
                 model_names = {
                     'load': [os.path.split(f.path)[-1] for f in os.scandir(os.path.join(self.model_group_path,'load')) if f.is_dir()],
-                    'solar': [os.path.split(f.path)[-1] for f in os.scandir(os.path.join(self.model_group_path,'solar')) if f.is_dir()],
+                    'solar': os.path.split(list(os.scandir(os.path.join(self.model_group_path,'solar')))[0])[-1],
                     'pricing': os.path.split(list(os.scandir(os.path.join(self.model_group_path,'pricing')))[0])[-1],
                     'carbon': os.path.split(list(os.scandir(os.path.join(self.model_group_path,'carbon')))[0])[-1]
                 }
@@ -200,10 +200,11 @@ class TF_Predictor(BasePredictorModel):
         """
 
         # check for duplicated model names, warn user, and create copies
-        for model_type in ['load','solar']:
+        for model_type in ['load']:
             for model_name in self.model_names[model_type]:
                 if self.model_names[model_type].count(model_name) > 1:
-                    warnings.warn("Warning: Repeated model names used. Multiple copies of models created. Please check `model_names` to make sure this is the desired behaviour.\n%s"%self.model_names)
+                    warnings.warn("Warning: Repeated model names used. Multiple copies of models created. Please check `model_names`\
+                                  to make sure this is the desired behaviour.\n%s"%self.model_names)
                     n_copies = 0
                     for i,name in enumerate(self.model_names[model_type]):
                         if name == model_name:
@@ -212,10 +213,12 @@ class TF_Predictor(BasePredictorModel):
 
         # NOTE: order of models in dictionary defines models used for prediction at each index in load and solar arrays
         self.models = { # ignore '#copyX' pattern in model name when loading
-            model_type: {model_name: self._load_model_from_dir_path(os.path.join(self.model_group_path,model_type,model_name.split('#')[0])) for model_name in self.model_names[model_type]} for model_type in ['load','solar']
+            model_type: {model_name: self._load_model_from_dir_path(os.path.join(self.model_group_path,model_type,model_name.split('#')[0]))\
+                for model_name in self.model_names[model_type]} for model_type in ['load']
         }
         self.models.update({
-            model_type: {self.model_names[model_type]: self._load_model_from_dir_path(os.path.join(self.model_group_path,model_type,self.model_names[model_type]))} for model_type in ['pricing','carbon']
+            model_type: {self.model_names[model_type]: self._load_model_from_dir_path(os.path.join(self.model_group_path,model_type,self.model_names[model_type]))}\
+                for model_type in ['solar','pricing','carbon']
         })
 
 
@@ -361,7 +364,7 @@ class TF_Predictor(BasePredictorModel):
             consistency. ALWAYS USE unless creating a new model.
             building_index (int, optional): Index of building in CityLearnEnv.buildings
             of target variable for dataset. Defaults to None. Required if target
-            variable is building specified, i.e. `model_type` is 'load' or 'solar'.
+            variable is building specified, i.e. `model_type` is 'load'.
 
         Returns:
             List[TimeSeriesDataSet]: List of formatted datasets created.
@@ -371,7 +374,7 @@ class TF_Predictor(BasePredictorModel):
 
         assert model_type in self.model_types, f"`model_type` argument must be one of {self.model_types}."
 
-        if (model_type in ['load','solar']) and (building_index == None):
+        if (model_type in ['load']) and (building_index == None):
             raise ValueError(f"Must supply `building_index` to construct {model_type} dataset from CityLearn data.")
 
         if model_name is not None:
@@ -480,7 +483,7 @@ class TF_Predictor(BasePredictorModel):
         model = self.pytorch_forecasting_model_class.from_dataset(train_dataset,**kwargs)
 
         # set model
-        if model_type in ['load','solar']:
+        if model_type in ['load']:
             self.model_names[model_type].append(model_name)
         else:
             warnings.warn("Warning: {} model replaced.".format(model_type))
@@ -614,7 +617,7 @@ class TF_Predictor(BasePredictorModel):
         # initialise observations buffer
         self.buffer = {
             'load': [[] for l in range(len(self.models['load'].keys()))],
-            'solar': [[] for l in range(len(self.models['solar'].keys()))],
+            'solar': [],
             'pricing': [],
             'carbon': []
         }
@@ -628,6 +631,9 @@ class TF_Predictor(BasePredictorModel):
         self.past_temps = env.buildings[0].weather.outdoor_dry_bulb_temperature
         self.past_dif_irads = env.buildings[0].weather.diffuse_solar_irradiance
         self.past_dir_irads = env.buildings[0].weather.direct_solar_irradiance
+
+        # get first building PV capacity for observation processing
+        self.b0_pv_cap = env.buildings[0].pv.nominal_power
 
         # put models in evaluation mode for prediction
         for model_type in self.models.keys():
@@ -658,8 +664,8 @@ class TF_Predictor(BasePredictorModel):
         Returns:
             predicted_loads (np.array): predicted electrical loads of buildings in each
                 period of the planning horizon (kWh) - shape (N,tau)
-            predicted_pv_gens (np.array): predicted energy generations of pv panels in each
-                period of the planning horizon (kWh) - shape (N,tau)
+            predicted_pv_gens (np.array): predicted normalised energy generations of pv
+                panels in each period of the planning horizon (W/kWp) - shape (tau)
             predicted_pricing (np.array): predicted grid electricity price in each period
                 of the planning horizon ($/kWh) - shape (tau)
             predicted_carbon (np.array): predicted grid electricity carbon intensity in each
@@ -668,13 +674,16 @@ class TF_Predictor(BasePredictorModel):
 
         assert hasattr(self, 'buffer'), "You must enter prediction mode by calling `self.initialise_forecasting(tau)` before forecasting can be performed."
         assert all([len(self.models[key])>0 for key in self.models.keys()]), "You must load models for all variables to perform prediction."
-        assert len(self.models['load']) == len(self.models['solar']) == np.array(observations).shape[0], "You must provide the same number of `load` and `solar` models as buildings being predicted for."
+        assert len(self.models['load']) == np.array(observations).shape[0], "You must provide the same number of `load` models as buildings being predicted for."
 
         # Update observation buffers.
         for model_type,obs_id in zip(self.model_types,[self.load_obs_index,self.solar_obs_index,self.pricing_obs_index,self.carbon_obs_index]):
-            if model_type in ['load','solar']:
+            if model_type in ['load']:
                 for j,val in enumerate(np.array(observations)[:, obs_id]):
                     self.buffer[model_type][j].append(val)
+            elif model_type in ['solar']:
+                # process solar observations to be in W/kWp - use 1st building as common to all
+                self.buffer[model_type].append(np.array(observations)[0, obs_id]*1000/self.b0_pv_cap)
             else: # pricing and carbon observations are shared between buildings
                 self.buffer[model_type].append(np.array(observations)[0, obs_id])
 
@@ -717,17 +726,14 @@ class TF_Predictor(BasePredictorModel):
             predicted_loads = np.array(predicted_loads)
 
             # perform solar forecasting
-            predicted_pv_gens = []
             model_type = 'solar'
-            for j,model_name in enumerate(self.model_names[model_type]):
-                model = self.models[model_type][model_name]
-                tsds_params = self.get_TimeSeriesDataSet_parameters(model_type,model_name)
-                data_df = base_df.copy()
-                data_df[self.solar_col_name] = np.append(self.buffer[model_type][j][-self.L:],np.zeros(self.T))
-                data_ds = TimeSeriesDataSet.from_parameters(tsds_params, data_df)
-                solar_prediction = np.array(model.predict(data_ds, mode='prediction')).reshape(self.T)[:self.tau]
-                predicted_pv_gens.append(solar_prediction)
-            predicted_pv_gens = np.array(predicted_pv_gens)
+            model_name = self.model_names[model_type]
+            model = self.models[model_type][model_name]
+            tsds_params = self.get_TimeSeriesDataSet_parameters(model_type,model_name)
+            data_df = base_df.copy()
+            data_df[self.solar_col_name] = np.append(self.buffer[model_type][-self.L:],np.zeros(self.T))
+            data_ds = TimeSeriesDataSet.from_parameters(tsds_params, data_df)
+            predicted_pv_gens = np.array(model.predict(data_ds, mode='prediction')).reshape(self.T)[:self.tau]
 
             # perform pricing forecasting
             model_type = 'pricing'

models/example/example_model.py
@@ -14,26 +14,22 @@ This class must have the following methods:
 You may wish to implement additional methods to make your model code neater.
 """
 
+from citylearn.citylearn import CityLearnEnv
 from models.base_predictor_model import BasePredictorModel
 import numpy as np
 
 
 class ExamplePredictor(BasePredictorModel):
 
-    def __init__(self, N: int, tau: int):
+    def __init__(self, N: int):
         """Initialise Prediction object and perform setup.
         
         Args:
             N (int): number of buildings in model, hence number of buildings
                 requiring forecasts.
-            tau (int): length of planning horizon (number of time instances
-                into the future to forecast).
-                Note: with some adjustment of the codebase variable length
-                planning horizons can be implemented.
         """
 
         self.num_buildings = N
-        self.tau = tau
 
         # Load in pre-computed prediction model.
         self.load()
@@ -55,6 +51,20 @@ class ExamplePredictor(BasePredictorModel):
         """No loading required for trivial example model."""
         pass
 
+    def initialise_forecasting(self, tau: int, env: CityLearnEnv):
+        """Initialise attributes required to perform forecasting.
+
+        Args:
+            tau (int): length of planning horizon (number of time instances
+                into the future to forecast).
+                Note: with some adjustment of the codebase variable length
+                planning horizons can be implemented.
+            env (CityLearnEnv): CityLearnEnvironment object.
+        """
+
+        self.tau = tau
+        self.b0_pv_cap = env.buildings[0].pv.nominal_power
+
     def compute_forecast(self, observations):
         """Compute forecasts given current observation.
 
@@ -67,8 +77,8 @@ class ExamplePredictor(BasePredictorModel):
         Returns:
             predicted_loads (np.array): predicted electrical loads of buildings in each
                 period of the planning horizon (kWh) - shape (N,tau)
-            predicted_pv_gens (np.array): predicted energy generations of pv panels in each
-                period of the planning horizon (kWh) - shape (N,tau)
+            predicted_pv_gens (np.array): predicted normalised energy generations of pv
+                panels in each period of the planning horizon (W/kWp) - shape (tau)
             predicted_pricing (np.array): predicted grid electricity price in each period
                 of the planning horizon ($/kWh) - shape (tau)
             predicted_carbon (np.array): predicted grid electricity carbon intensity in each
@@ -83,14 +93,14 @@ class ExamplePredictor(BasePredictorModel):
         # ====================================================================
         current_vals = {
             'loads': np.array(observations)[:,20],
-            'pv_gens': np.array(observations)[:,21],
+            'pv_gens': np.array(observations)[0,21]*1000/self.b0_pv_cap,
             'pricing': np.array(observations)[0,24],
             'carbon': np.array(observations)[0,19]
         }
 
         if self.prev_vals['carbon'] is None:
             predicted_loads = np.repeat(current_vals['loads'].reshape(self.num_buildings,1),self.tau,axis=1)
-            predicted_pv_gens = np.repeat(current_vals['pv_gens'].reshape(self.num_buildings,1),self.tau,axis=1)
+            predicted_pv_gens = np.repeat(current_vals['pv_gens'],self.tau)
             predicted_pricing = np.repeat(current_vals['pricing'],self.tau)
             predicted_carbon = np.repeat(current_vals['carbon'],self.tau)
 
@@ -101,8 +111,7 @@ class ExamplePredictor(BasePredictorModel):
             load_lines = [np.poly1d(np.polyfit([-1,0],[self.prev_vals['loads'][b],current_vals['loads'][b]],deg=1)) for b in range(self.num_buildings)]
             predicted_loads = np.array([line(predict_inds) for line in load_lines]).clip(0.01)
 
-            pv_gen_lines = [np.poly1d(np.polyfit([-1,0],[self.prev_vals['pv_gens'][b],current_vals['pv_gens'][b]],deg=1)) for b in range(self.num_buildings)]
-            predicted_pv_gens = np.array([line(predict_inds) for line in pv_gen_lines]).clip(0)
+            predicted_pricing = np.poly1d(np.polyfit([-1,0],[self.prev_vals['pv_gens'],current_vals['pv_gens']],deg=1))(predict_inds).clip(0.01)
 
             predicted_pricing = np.poly1d(np.polyfit([-1,0],[self.prev_vals['pricing'],current_vals['pricing']],deg=1))(predict_inds).clip(0.01)
 

models/noise/NoisePredictor.py
@@ -25,8 +25,8 @@ class GRWN_Predictor(BasePredictorModel):
                 planning horizons can be implemented.
             noise_levels (dict): dictionary of noise levels to use for each
                 parameter. Values are the proprotional noise level, in range
-                [0,1]. Keys are `load`, `solar`, `pricing`, `carbon`. Values
-                for `load` and `solar` are dicts where the keys are the names
+                [0,1]. Keys are `load`, `solar`, `pricing`, `carbon`. For
+                `load` the value is a dict where the keys are the names
                 of the buildings (from env.buildings), and the values are the
                 noise levels for each buildling.
         """
@@ -38,7 +38,7 @@ class GRWN_Predictor(BasePredictorModel):
 
         self.param_means = {
             'load': {b.name: np.mean(b.energy_simulation.non_shiftable_load) for b in self.env.buildings},
-            'solar' : {b.name: np.mean(b.pv.get_generation(b.energy_simulation.solar_generation)) for b in self.env.buildings},
+            'solar' : np.mean(self.env.buildings[0].energy_simulation.solar_generation),
             'pricing': np.mean(self.env.buildings[0].pricing.electricity_pricing),
             'carbon': np.mean(self.env.buildings[0].carbon_intensity.carbon_intensity)
         }
@@ -55,7 +55,7 @@ class GRWN_Predictor(BasePredictorModel):
         if param_type == 'load':
             return np.array([self.grwn(self.param_means['load'][b.name] * self.noise_levels['load'][b.name]) for b in self.env.buildings])
         elif param_type == 'solar':
-            return np.array([self.grwn(self.param_means['solar'][b.name] * self.noise_levels['solar'][b.name]) for b in self.env.buildings])
+            return self.grwn(self.param_means['solar'] * self.noise_levels['solar'])
         elif param_type == 'pricing':
             return self.grwn(self.param_means['pricing'] * self.noise_levels['pricing'])
         elif param_type == 'carbon':
@@ -76,8 +76,8 @@ class GRWN_Predictor(BasePredictorModel):
         Returns:
             predicted_loads (np.array): predicted electrical loads of buildings in each
                 period of the planning horizon (kWh) - shape (N,tau)
-            predicted_pv_gens (np.array): predicted energy generations of pv panels in each
-                period of the planning horizon (kWh) - shape (N,tau)
+            predicted_pv_gens (np.array): predicted normalised energy generations of pv
+                panels in each period of the planning horizon (W/kWp) - shape (tau)
             predicted_pricing (np.array): predicted grid electricity price in each period
                 of the planning horizon ($/kWh) - shape (tau)
             predicted_carbon (np.array): predicted grid electricity carbon intensity in each
@@ -91,8 +91,8 @@ class GRWN_Predictor(BasePredictorModel):
                 [b.energy_simulation.non_shiftable_load[t+1:t+self.tau+1]\
                     for b in self.env.buildings]) + self.param_grwn('load')
             predicted_pv_gens = np.array(
-                [b.pv.get_generation(b.energy_simulation.solar_generation)[t+1:t+self.tau+1]\
-                    for b in self.env.buildings]) + self.param_grwn('solar')
+                self.env.buildings[0].energy_simulation.solar_generation[t+1:t+self.tau+1])\
+                    + self.param_grwn('solar')
             predicted_pricing = np.array(
                 self.env.buildings[0].pricing.electricity_pricing[t+1:t+self.tau+1])\
                     + self.param_grwn('pricing')