6004170839fb21a2dbdbac7d59bad7f3325ae617
mal84emma <mal84@cam.ac.uk>
2023-11-27 15:44:53 +0000

    Upload model generalisation results

--- experiments/generalisation_performance.py
+++ experiments/generalisation_performance.py
@@ -20,11 +20,11 @@ from models import (
 if __name__ == "__main__":
 
     # Run using
-    # for ($m = 0; $m -le 5; $m++) {for ($bid = 0; $bid -le 14; $bid++) {python -m experiments.generalisation_performance $m $bid}}
+    # for ($m = 0; $m -le 4; $m++) {for ($bid = 0; $bid -le 14; $bid++) {python -m experiments.generalisation_performance $m $bid}}
     # ==================================================================================================
 
     m = int(sys.argv[1])
-    b_id = int(sys.argv[2])
+    b = int(sys.argv[2])
 
     UCam_ids = [0,3,9,11,12,15,16,25,26,32,38,44,45,48,49] # set as list of same int to test model on different buildings
 
@@ -41,10 +41,10 @@ if __name__ == "__main__":
     model_name = model_names[m]
     expt_name = expt_names[m]
     predictor_type = predictor_types[m]
-    train_building_index = b_id
+    train_building_index = UCam_ids[b]
 
     if predictor_type in [TFT_Predictor,NHiTS_Predictor,DeepAR_Predictor]:
-        predictor = predictor_type(model_group_name=expt_name, model_names=[b_id]*len(UCam_ids))
+        predictor = predictor_type(model_group_name=expt_name, model_names=[train_building_index]*len(UCam_ids))
     elif predictor_type in [DMSPredictor]:
         predictor = predictor_type(building_indices=UCam_ids, expt_name=os.path.join('analysis',expt_name), load=True)
 
@@ -56,7 +56,7 @@ if __name__ == "__main__":
 
     results.update({
         'model_name': model_name,
-        'train_building_index': b_id,
+        'train_building_index': train_building_index,
         'tau': tau
         })
 
